e0799b1dc9719f2a5c8f2b17750c1e5b04c45a1c
LesyaK5 <letters-for-work@yandex.ru>
2024-08-16 16:39:19 +0500

    Delete alana/server directory

--- alana/server/getProductCategories.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'db.php';
-
-$sql = $pdo->prepare("SELECT * FROM product_category");
-$sql->execute();
-$result = $sql->fetchAll(PDO::FETCH_OBJ);
-$result = json_encode($result);
-print_r($result);
